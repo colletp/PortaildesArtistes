d8e3373a34bb52af91aa3654166aa40e88bebd9c
collet@copagest.be <4PVA9YF6$>
2019-03-14 18:13:40 +0100

    Mise a jour des soruces : création d'une interface pour le service et correction d'un problème de démarrage du controller d'authentification

--- wsartiste/src/com/unamur/portaildesartistes/wsartiste/wsfrontend/contrat/AuthentificationService2.java
+++ wsartiste/src/com/unamur/portaildesartistes/wsartiste/wsfrontend/contrat/AuthentificationService2.java
@@ -1,31 +1,30 @@
-package com.unamur.portaildesartistes.wsartiste.gestionutilisateur;
+package com.unamur.portaildesartistes.wsartiste.wsfrontend.contrat;
 
 import com.unamur.portaildesartistes.wsartiste.corelayer.AuthentificationBean;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
 
 @RestController
-public class AuthentificationController {
+public class AuthentificationService2 {
 
-    private static final Logger logger = LoggerFactory.getLogger(AuthentificationController.class);
+    private static final Logger logger = LoggerFactory.getLogger(AuthentificationService2.class);
     private static int i;
 
-    AuthentificationServiceImpl f_authentificationServiceImpl;
+    private com.unamur.portaildesartistes.wsartiste.gestionutilisateur.AuthentificationServiceImpl f_authentificationServiceImpl;
 
-    @GetMapping("wsartiste/gestionUtilisateur/list")
+    @GetMapping("/gestionUtilisateur/list")
     public List<AuthentificationBean> list() {
         return f_authentificationServiceImpl.list();
     }
 
-    @GetMapping("wsartiste/gestionUtilisateur/insertOK")
+    @GetMapping("/gestionUtilisateur/insertOK")
     public Integer insertOK() {
         return f_authentificationServiceImpl.insertOK();
     }
