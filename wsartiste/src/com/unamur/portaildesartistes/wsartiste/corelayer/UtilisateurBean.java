bd2f57d12870e23e4f2351b376dcf71d95f95eb9
collet@copagest.be <4PVA9YF6$>
2019-03-17 13:28:18 +0100

    Mise a jour des soruces : - Ajout de propriété sur le bean Utilisateur.

--- wsartiste/src/com/unamur/portaildesartistes/wsartiste/corelayer/UtilisateurBean.java
+++ wsartiste/src/com/unamur/portaildesartistes/wsartiste/corelayer/UtilisateurBean.java
@@ -1,19 +1,52 @@
 package com.unamur.portaildesartistes.wsartiste.corelayer;
 
+import com.unamur.portaildesartistes.wsartiste.datalayer.DonneeUtilisateur;
+
 import java.io.Serializable;
 
 public class UtilisateurBean implements Serializable {
 
+    // ******************
+    // Champs/propriétés
+    // ******************
+
     private Integer id;
     private String nomUtilisateur;
+    private String login;
+    private String motDePasse;
+    private boolean actif;
 
-    public Integer getId() {
-        return id;
+    // ******************
+    // Constructeur
+    // ******************
+
+    public UtilisateurBean() {
+        super();
     }
 
-    public void setId(Integer id) {
-        this.id = id;
+    public UtilisateurBean(String p_nomUtilisateur, String p_login, String p_motDePasse, boolean p_actif) {
+        this.nomUtilisateur = p_nomUtilisateur;
+        this.login = p_login;
+        this.motDePasse = p_motDePasse;
+        this.actif = p_actif;
     }
+
+    public UtilisateurBean(DonneeUtilisateur p_donneeUtilisateur) {
+        /*
+        this.nomUtilisateur = p_donneeUtilisateur;
+        this.login = p_donneeUtilisateur;
+        this.motDePasse = p_donneeUtilisateur;
+        this.actif = p_donneeUtilisateur.;
+        */
+    }
+
+    // ******************
+    // Setter/Getter
+    // ******************
+
+    public Integer getId() { return id; }
+
+    public void setId( Integer p_id) { this.id = p_id; }
 
     public String getNomUtilisateur() {
         return nomUtilisateur;
@@ -22,4 +55,9 @@ public class UtilisateurBean implements Serializable {
     public void setNomUtilisateur(String nomUtilisateur) {
         this.nomUtilisateur = nomUtilisateur;
     }
+
+    // ******************
+    // Fonctions
+    // ******************
+
 }
