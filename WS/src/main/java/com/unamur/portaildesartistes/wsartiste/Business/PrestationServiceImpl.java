0f0e77672e08e0794ac11fecab2c4185f17beda3
collet@copagest.be <4PVA9YF6$>
2019-05-09 21:43:50 +0200

    DocArtiste => Backend

--- WS/src/main/java/com/unamur/portaildesartistes/wsartiste/Business/PrestationServiceImpl.java
+++ WS/src/main/java/com/unamur/portaildesartistes/wsartiste/Business/PrestationServiceImpl.java
@@ -6,11 +6,13 @@ import com.unamur.portaildesartistes.wsartiste.datalayer.DonneePrestationImpl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.UUID;
 
+@Service
 public class PrestationServiceImpl implements IService<PrestationDTO> {
 
     private static final Logger logger = LoggerFactory.getLogger(PrestationServiceImpl.class);
